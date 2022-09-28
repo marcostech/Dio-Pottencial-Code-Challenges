686a9250f0057f638dc212b5deeb641706b69610
Marcos Vinicius <54728070+marcostech@users.noreply.github.com>
2022-09-28 04:00:35 -0300

    Add files via upload

---
+++ desafio1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Dio
+{
+    private static void Main(string[] args)
+    {
+        int numero = 0;
+        bool conversao = false;
+        do
+        {
+            //Console.WriteLine("Escreva um número inteiro, maior que zero e menor que 10^5");
+            conversao = int.TryParse(Console.ReadLine(), out numero);
+            /*
+            if (numero <= 0 || !conversao)
+            {
+                Console.WriteLine("Erro, número DEVE ser maior que zero e menor que 10^5");
+            }
+            */
+        } while (numero <= 0 || !conversao);
+
+        int produto = 1, soma = 0;
+        while (numero > 0)
+        {
+            int digito = numero % 10;            
+            // TODO: Crie as outras condições necessárias para a resolução do desafio:
+            produto *= digito;
+            soma += digito;
+            numero /= 10;
+        }
+
+        Console.WriteLine(produto - soma);
+    }
+}
